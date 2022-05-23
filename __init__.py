ee79eba08d83399702ae7b00ac43facd8a9f6ac2
nixs-dev <joabewick@gmail.com>
2022-05-22 22:41:16 -0300

    V1

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ def setup_tree(path):
 		path_splitted = path.split("/")
 		dir_name = path_splitted[-1] if path_splitted[-1] != "" else path_splitted[-2]
 		
-		return Content(path, dir_name, 0, "Folder")
+		return Content(path, dir_name, 0, 0, "Folder")
 	else:
 		return "Diretório não encontrado!"
 		
